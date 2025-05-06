37a573a7a91ccf7f7f21f925539dd733a4db1272
Fer Luna @ ISTEA && Educación IT <128251567+FernandoIstea@users.noreply.github.com>
2025-05-06 17:48:02 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,30 +7,13 @@ const videos = [
 const video = document.querySelector("video")
 const videoName = document.querySelector("p#videoName")
 
-/* 
-    // agregamos a la ecuación el análisis de la batería del dispositivo
-
-    let batteryLevel = 100
-
-    navigator.getBattery().then((result)=> {
-        batteryLevel = result.level * 100
-        console.log(batteryLevel)
-    })
-
-    if (batteryLevel > 25) {
-        // speed = "2g" // simulamos que la conexión es de 2G para enviar un video de menor calidad
-    } else {
-        const speed = navigator.connection.effectiveType
-    }
-*/
-
 function cargarVideoXvelocidad() {
     const speed = navigator.connection.effectiveType
     const media = videos.find((v)=> v.speed === speed)
 
     videoName.textContent = media.tag
     video.src = media.video
-    video.play()
+    // video.play()
 }
 
 cargarVideoXvelocidad()
